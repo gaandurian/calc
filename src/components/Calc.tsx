2704b429e6556b4910c0e75905dc88e7f81687a2
Mahmoud Ghandour <ghan.mahmoud@gmail.com>
2023-04-12 20:04:33 +0100

    Removed Mathjs dependency and replaced it with my logic.

--- src/components/Calc.tsx
+++ src/components/Calc.tsx
@@ -1,94 +1,143 @@
-import { useState } from "react"
-import NumberButton from "./NumberButton"
-import OperatorButton from "./OperatorButton"
-import * as math from 'mathjs'
+/**
+ * I previously used Mathjs library to handle the simple math calculations
+ * however the only reason behind that was that I was short on time and I thought
+ * to myself this is a design problem more than logic.
+ * So I decided to implement some simple logic for it.
+ * I wouldve loved to do some unit testing but I don't want to exceed an hour on this task.
+ */
+
+import { useEffect, useState } from "react";
+import NumberButton from "./NumberButton";
+import OperatorButton from "./OperatorButton";
+
+const operations = new Map([
+  ["add", (a: number, b: number) => a + b],
+  ["subtract", (a: number, b: number) => a - b],
+  ["multiply", (a: number, b: number) => a * b],
+  ["divide", (a: number, b: number) => a / b],
+]);
 
 const Calc = () => {
- 
-  const [expression, setExpression] = useState<string>('')
-  const [result, setResult] = useState<string>('')
+  const [result, setResult] = useState<string>("");
+  const [lcdValue, setLcdValue] = useState<string>("");
+  const [stack, setStack] = useState<string[]>([]);
 
   const handleUpdateExpression = (value: string) => {
-    setExpression(prev => prev.concat(value))
-  }
-
-  const handleCalculateResult = (expr: string) => {
-    let result
-    try {
-      result = math.evaluate(expr)
-    } catch (error) {
-      console.log(error)
+    if (result !== "") {
+      setLcdValue(value);
+      setResult("");
       return;
     }
-      setResult(result)
-  }
+    setLcdValue((prev) => prev + value);
+  };
+  const handleOperatorPress = (operator: string, lcdVal: string) => {
+    if (lcdValue === "") {
+      if (operator === "equals") return; // No value on the screen and the user presses =, do nothing
+      if (stack.length === 2) {
+        // No value in the lcd and the user presses an operator, update operator
+        setStack((prev) => {
+          // update the operator in the stack
+          const [a] = prev;
+          return [a, operator];
+        });
+      }
+    }
+
+    if (stack.length === 2) {
+      setStack((prev) => {
+        const [a, op] = prev;
+
+        const calculateOp = operations.get(op);
+        if (!calculateOp) return prev;
+
+        const result = calculateOp(Number(a), Number(lcdValue));
+        setLcdValue(result.toString());
+        setResult(result.toString());
+        return operator === "equals"
+          ? [result.toString()]
+          : [result.toString(), operator];
+      });
+      return;
+    }
+
+    if (operator === "equals") return; // If there is no operator in the stack and the user presses equals, do nothing
+    setStack([lcdValue, operator]); // If there is no operator in the stack and the user presses an operator, add the operator to the stack
+    setLcdValue("");
+  };
+
+  const clear = () => {
+    setLcdValue("");
+    setStack([]);
+    setResult("");
+  };
+
+  useEffect(() => console.log(stack), [stack]);
 
   const CALC = [
-    {
-      number: 7,
-    },
-    {
-      number: 8,
-    },
-    {
-      number: 9,
-    },
-    {
-      number: 4,
-    },
-    {
-      number: 5,
-    },
-    {
-      number: 6,
-    },
-    {
-      number: 1,
-    },
-    {
-      number: 2,
-    },
-    {
-      number: 3,
-    },
-    {
-      number: 0,
-      large: true
-    },
-
-
-  ]
+    { number: 7 },
+    { number: 8 },
+    { number: 9 },
+    { number: 4 },
+    { number: 5 },
+    { number: 6 },
+    { number: 1 },
+    { number: 2 },
+    { number: 3 },
+    { number: 0, large: true },
+  ];
   return (
-    <div className="flex flex-col gap-4 bg-stone-200 rounded-lg p-3">
-
+    <div className="flex flex-col gap-4 bg-stone-200 rounded-xl p-3">
       {/* Calculator Monitor or screen */}
-      <div className="h-20 w-full bg-white rounded-lg font-extrabold text-3xl p-2">
-        {expression}
-        <div className="text-cyan-600 flex justify-end">{result}</div>
+      <div className="h-20 w-full bg-white rounded-lg p-2 flex justify-end items-end">
+        <div className="text-cyan-600 text-5xl font-extrabold"> {lcdValue}</div>
       </div>
 
+      <div className="flex gap-2">
+        <div className="flex gap-2">
+          {/* Calculator Numbers  */}
+          <div className="grid grid-cols-3 grid-rows-3 gap-2">
+          <OperatorButton className="col-span-3" onClick={() => clear()} operator="C" />
+            {CALC.map((item, index) => (
+              <NumberButton
+                key={index}
+                handleUpdateExpression={() =>
+                  handleUpdateExpression(item.number.toString())
+                }
+                number={item.number}
+                large={item.large}
+              />
+            ))}
+            </div>
+          </div>
 
-      <div className="flex gap-2">   
-        {/* Calculator Numbers  */}
-      <div className="grid grid-cols-3 gap-2">
-        {CALC.map((item, index) => <NumberButton 
-        key={index}
-        handleUpdateExpression={() => handleUpdateExpression(item.number.toString())} 
-        number={item.number} 
-        large={item.large} />)}
+          {/* Calc operators */}
+          <div className="flex flex-col gap-y-2">
+            <OperatorButton
+              onClick={() => handleOperatorPress("divide", lcdValue)}
+              operator="÷"
+            />
+            <OperatorButton
+              onClick={() => handleOperatorPress("multiply", lcdValue)}
+              operator="×"
+            />
+            <OperatorButton
+              onClick={() => handleOperatorPress("subtract", lcdValue)}
+              operator="-"
+            />
+            <OperatorButton
+              onClick={() => handleOperatorPress("add", lcdValue)}
+              operator="+"
+            />
+            <OperatorButton
+              className="bg-orange-500"
+              operator="="
+              onClick={() => handleOperatorPress("equals", lcdValue)}
+            />
+
+        </div>
       </div>
-
-      {/* Calc operators */}
-     <div className="flex flex-col gap-y-2">
-       <OperatorButton onClick={() => handleUpdateExpression('/')} operator="÷" /> 
-       <OperatorButton onClick={() => handleUpdateExpression('*')} operator="×" /> 
-       <OperatorButton onClick={() => handleUpdateExpression('+')} operator="+" />
-       <OperatorButton onClick={() => handleCalculateResult(expression)}  operator="=" />
-     </div>
-
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Calc
+export default Calc;
--- src/components/OperatorButton.tsx
+++ src/components/OperatorButton.tsx
@@ -1,12 +1,13 @@
 
-interface OperatorButtonProps {
+interface OperatorButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
     operator: string,
     onClick?: any;
+    className?: string;
 }
 
-const OperatorButton: React.FC<OperatorButtonProps> = ({ operator, onClick }) => {
+const OperatorButton: React.FC<OperatorButtonProps> = ({ operator, onClick, className, ...rest } ) => {
   return (
-    <button onClick={onClick} className="rounded-lg h-16 w-16 bg-cyan-600 text-white font-extrabold text-5xl">
+    <button onClick={onClick} className={`rounded-lg h-16 w-16 bg-cyan-600 text-white font-extrabold text-5xl ${className}`} {...rest}>
         {operator}
     </button>
   )
